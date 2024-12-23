b250fbe35e600055fae5fd803fe2d9ad920b9ca0
yybacik <yusufbacik@icloud.com>
2024-12-23 14:15:14 +0300

    Activities were created for the profile page

---
+++ app/src/main/java/com/atlantis/bankan/p_SettingsActivity.kt
@@ -0,0 +1,20 @@
+package com.atlantis.bankan
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class p_SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_psettings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
